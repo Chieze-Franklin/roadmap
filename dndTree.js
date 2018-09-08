d766acddd00dfdc7090e868d8e24459c3293ad23
Franklin Chieze <chieze.franklin@gmail.com>
2018-09-08 18:35:52 +0100

    style the tooltip [take 4]

--- dndTree.js
+++ dndTree.js
@@ -351,19 +351,19 @@ dragListener = d3.behavior.drag()
 
   function mouseover(d) {
     if (d3.event.defaultPrevented) return; // mouseover suppressed
-    console.log("build 25");
+    console.log("build 26");
     tooltipDivElement.style.left = `${d3.event.pageX - 25}px`;
     tooltipDivElement.style.top = `${d3.event.pageY - 10}px`;
 
     var linksHtml = d.links ? d.links.map(l => `<li><a href="${l}">${l}</a></li>`) : '';
     var titleB = `<b>${d.name}</b>`;
     var titleP = `<p class="title">${titleB}</p>`;
-    var innerLinksDiv = `<div class="disclaimer">links: ${d.links? `<ul>${linksHtml.join('')}</ul>`: ''}</div>`;
+    var innerLinksDiv = d.links ? `<div class="disclaimer">links: <ul>${linksHtml.join('')}</ul></div>` : '';
     var linksDiv = `<div><br />${innerLinksDiv}</div>`;
     var infoDiv = `<div class="information">${d.description || ''} ${linksDiv}</div>`;
     var tooltipDiv = `<div class="tooltip-module">
     ${titleP}
-    <br />
+    <hr />
     ${infoDiv}
     </div>`;
 
