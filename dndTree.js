a707fcde974b8c58249b1affa3c624d6dcbcf065
Franklin Chieze <chieze.franklin@gmail.com>
2018-09-09 18:20:00 +0100

    modify /Robotics & IoT node

--- dndTree.js
+++ dndTree.js
@@ -377,181 +377,181 @@ dragListener = d3.behavior.drag()
     if (d3.event.defaultPrevented) return; // mouseout suppressed
   }
 
-function update(source) {
-  // Compute the new height, function counts total children of root node and sets tree height accordingly.
-  // This prevents the layout looking squashed when new nodes are made visible or looking sparse when nodes are removed
-  // This makes the layout more consistent.
-  var levelWidth = [1];
-  var childCount = function(level, n) {
+    function update(source) {
+        // Compute the new height, function counts total children of root node and sets tree height accordingly.
+        // This prevents the layout looking squashed when new nodes are made visible or looking sparse when nodes are removed
+        // This makes the layout more consistent.
+        var levelWidth = [1];
+        var childCount = function(level, n) {
 
-      if (n.children && n.children.length > 0) {
-          if (levelWidth.length <= level + 1) levelWidth.push(0);
+            if (n.children && n.children.length > 0) {
+                if (levelWidth.length <= level + 1) levelWidth.push(0);
 
-          levelWidth[level + 1] += n.children.length;
-          n.children.forEach(function(d) {
-              childCount(level + 1, d);
-          });
-      }
-  };
-  childCount(0, root);
-  var newHeight = d3.max(levelWidth) * 25; // 25 pixels per line  
-  tree = tree.size([newHeight, viewerWidth]);
-
-  // Compute the new tree layout.
-  var nodes = tree.nodes(root).reverse(),
-      links = tree.links(nodes);
-
-  // Set widths between levels based on maxLabelLength.
-  nodes.forEach(function(d) {
-      d.y = (d.depth * (maxLabelLength * 10)); //maxLabelLength * 10px
-      // alternatively to keep a fixed scale one can set a fixed depth per level
-      // Normalize for fixed-depth by commenting out below line
-      // d.y = (d.depth * 500); //500px per level.
-  });
-
-  // Update the nodes…
-  node = svgGroup.selectAll("g.node")
-      .data(nodes, function(d) {
-          return d.id || (d.id = ++i);
-      });
-
-      // Enter any new nodes at the parent's previous position.
-      var nodeEnter = node.enter().append("g")
-      .call(dragListener)
-      .attr("id", function(d) {
-        return d.id;
-      })
-      .attr("class", "node")
-      .attr("transform", function(d) {
-          return "translate(" + source.y0 + "," + source.x0 + ")";
-      })
-      .on('click', click)
-      .on('mouseout', mouseout)
-      .on('mouseover', mouseover);
-
-  nodeEnter.append("circle")
-      .attr('class', 'nodeCircle')
-      .attr("r", 0)
-      .style("fill", function(d) {
-          return d._children ? "lightsteelblue" : "#fff";
-      });
-
-  nodeEnter.append("text")
-      .attr("x", function(d) {
-          return d.children || d._children ? -10 : 10;
-      })
-      .attr("dy", ".35em")
-      .attr('class', 'nodeText')
-      .attr("text-anchor", function(d) {
-          return d.children || d._children ? "end" : "start";
-      })
-      .text(function(d) {
-          return d.name;
-      })
-      .style("fill-opacity", 0);
-
-  // phantom node to give us mouseover in a radius around it
-  nodeEnter.append("circle")
-      .attr('class', 'ghostCircle')
-      .attr("r", 30)
-      .attr("opacity", 0.2) // change this to zero to hide the target area
-  .style("fill", "red")
-      .attr('pointer-events', 'mouseover')
-      .on("mouseover", function(node) {
-          overCircle(node);
-      })
-      .on("mouseout", function(node) {
-        outCircle(node);
-    });
-
-// Update the text to reflect whether node has children or not.
-node.select('text')
-    .attr("x", function(d) {
-        return d.children || d._children ? -10 : 10;
-    })
-    .attr("text-anchor", function(d) {
-        return d.children || d._children ? "end" : "start";
-    })
-    .text(function(d) {
-        return d.name;
-    });
-
-// Change the circle fill depending on whether it has children and is collapsed
-node.select("circle.nodeCircle")
-    .attr("r", 4.5)
-    .style("fill", function(d) {
-        return d._children ? "lightsteelblue" : "#fff";
-    });
-
-// Transition nodes to their new position.
-var nodeUpdate = node.transition()
-    .duration(duration)
-    .attr("transform", function(d) {
-        return "translate(" + d.y + "," + d.x + ")";
-    });
-
-// Fade the text in
-nodeUpdate.select("text")
-    .style("fill-opacity", 1);
-    // Transition exiting nodes to the parent's new position.
-    var nodeExit = node.exit().transition()
-    .duration(duration)
-    .attr("transform", function(d) {
-        return "translate(" + source.y + "," + source.x + ")";
-    })
-    .remove();
-
-nodeExit.select("circle")
-    .attr("r", 0);
-
-nodeExit.select("text")
-    .style("fill-opacity", 0);
-
-// Update the links…
-var link = svgGroup.selectAll("path.link")
-    .data(links, function(d) {
-        return d.target.id;
-    });
-
-// Enter any new links at the parent's previous position.
-link.enter().insert("path", "g")
-    .attr("class", "link")
-    .attr("d", function(d) {
-        var o = {
-            x: source.x0,
-            y: source.y0
+                levelWidth[level + 1] += n.children.length;
+                n.children.forEach(function(d) {
+                    childCount(level + 1, d);
+                });
+            }
         };
-        return diagonal({
-            source: o,
-            target: o
-        });
-    });
-    // Transition links to their new position.
-    link.transition()
-    .duration(duration)
-    .attr("d", diagonal);
+        childCount(0, root);
+        var newHeight = d3.max(levelWidth) * 25; // 25 pixels per line  
+        tree = tree.size([newHeight, viewerWidth]);
 
-// Transition exiting nodes to the parent's new position.
-link.exit().transition()
-    .duration(duration)
-    .attr("d", function(d) {
-        var o = {
-            x: source.x,
-            y: source.y
-        };
-        return diagonal({
-            source: o,
-            target: o
-        });
-    })
-    .remove();
+        // Compute the new tree layout.
+        var nodes = tree.nodes(root).reverse(),
+            links = tree.links(nodes);
 
-// Stash the old positions for transition.
-nodes.forEach(function(d) {
-    d.x0 = d.x;
-    d.y0 = d.y;
-});
-}
+        // Set widths between levels based on maxLabelLength.
+        nodes.forEach(function(d) {
+            d.y = (d.depth * (maxLabelLength * 10)); //maxLabelLength * 10px
+            // alternatively to keep a fixed scale one can set a fixed depth per level
+            // Normalize for fixed-depth by commenting out below line
+            // d.y = (d.depth * 500); //500px per level.
+        });
+
+        // Update the nodes…
+        node = svgGroup.selectAll("g.node")
+            .data(nodes, function(d) {
+                return d.id || (d.id = ++i);
+            });
+
+            // Enter any new nodes at the parent's previous position.
+            var nodeEnter = node.enter().append("g")
+            .call(dragListener)
+            .attr("id", function(d) {
+                return d.id;
+            })
+            .attr("class", "node")
+            .attr("transform", function(d) {
+                return "translate(" + source.y0 + "," + source.x0 + ")";
+            })
+            .on('click', click)
+            .on('mouseout', mouseout)
+            .on('mouseover', mouseover);
+
+        nodeEnter.append("circle")
+            .attr('class', 'nodeCircle')
+            .attr("r", 0)
+            .style("fill", function(d) {
+                return d._children ? "lightsteelblue" : "#fff";
+            });
+
+        nodeEnter.append("text")
+            .attr("x", function(d) {
+                return d.children || d._children ? -10 : 10;
+            })
+            .attr("dy", ".35em")
+            .attr('class', 'nodeText')
+            .attr("text-anchor", function(d) {
+                return d.children || d._children ? "end" : "start";
+            })
+            .text(function(d) {
+                return d.name;
+            })
+            .style("fill-opacity", 0);
+
+        // phantom node to give us mouseover in a radius around it
+        nodeEnter.append("circle")
+            .attr('class', 'ghostCircle')
+            .attr("r", 30)
+            .attr("opacity", 0.2) // change this to zero to hide the target area
+        .style("fill", "red")
+            .attr('pointer-events', 'mouseover')
+            .on("mouseover", function(node) {
+                overCircle(node);
+            })
+            .on("mouseout", function(node) {
+                outCircle(node);
+            });
+
+        // Update the text to reflect whether node has children or not.
+        node.select('text')
+            .attr("x", function(d) {
+                return d.children || d._children ? -10 : 10;
+            })
+            .attr("text-anchor", function(d) {
+                return d.children || d._children ? "end" : "start";
+            })
+            .text(function(d) {
+                return d.name;
+            });
+
+        // Change the circle fill depending on whether it has children and is collapsed
+        node.select("circle.nodeCircle")
+            .attr("r", 4.5)
+            .style("fill", function(d) {
+                return d._children ? "lightsteelblue" : "#fff";
+            });
+
+        // Transition nodes to their new position.
+        var nodeUpdate = node.transition()
+            .duration(duration)
+            .attr("transform", function(d) {
+                return "translate(" + d.y + "," + d.x + ")";
+            });
+
+        // Fade the text in
+        nodeUpdate.select("text")
+            .style("fill-opacity", 1);
+            // Transition exiting nodes to the parent's new position.
+            var nodeExit = node.exit().transition()
+            .duration(duration)
+            .attr("transform", function(d) {
+                return "translate(" + source.y + "," + source.x + ")";
+            })
+            .remove();
+
+        nodeExit.select("circle")
+            .attr("r", 0);
+
+        nodeExit.select("text")
+            .style("fill-opacity", 0);
+
+        // Update the links…
+        var link = svgGroup.selectAll("path.link")
+            .data(links, function(d) {
+                return d.target.id;
+            });
+
+        // Enter any new links at the parent's previous position.
+        link.enter().insert("path", "g")
+            .attr("class", "link")
+            .attr("d", function(d) {
+                var o = {
+                    x: source.x0,
+                    y: source.y0
+                };
+                return diagonal({
+                    source: o,
+                    target: o
+                });
+            });
+            // Transition links to their new position.
+            link.transition()
+            .duration(duration)
+            .attr("d", diagonal);
+
+        // Transition exiting nodes to the parent's new position.
+        link.exit().transition()
+            .duration(duration)
+            .attr("d", function(d) {
+                var o = {
+                    x: source.x,
+                    y: source.y
+                };
+                return diagonal({
+                    source: o,
+                    target: o
+                });
+            })
+            .remove();
+
+        // Stash the old positions for transition.
+        nodes.forEach(function(d) {
+            d.x0 = d.x;
+            d.y0 = d.y;
+        });
+    }
 
 // Append a group which holds all nodes and which the zoom Listener can act upon.
 var svgGroup = baseSvg.append("g");
